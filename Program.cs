5f1123c855872a9a84d5a693aa7664e6d6fc5b95
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-10 07:55:24 +0530

    [Prudhvi] Add . Creating binary tree

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to binary search tree!");
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>();
+            binarySearchTree.InsertData(56);
+            binarySearchTree.InsertData(30);
+            binarySearchTree.InsertData(70);
+            binarySearchTree.Display(binarySearchTree.GetRoot());
+         
         }
     }
 }
